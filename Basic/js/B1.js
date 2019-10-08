7e74a3558388d4fa8b750861e7b925ff617a6f57
Barbara Anissin <anissinb@gmail.com>
2019-10-08 10:43:07 +0200

    something

--- Basic/js/B1.js
+++ Basic/js/B1.js
@@ -1,8 +1,15 @@
 function ages(year) {
-	var thisyear = new Date().getFullYear();
-	var age = thisyear - year
-	document.write(age)
+    var thisyear = new Date().getFullYear();
+    var age = thisyear - year
+    document.write(age)
 }
 
 ages(1999);
 
+
+function hero(name, cut) {
+    var res = name.substring(0,12);
+    console.log(res);
+}
+
+hero ('I am a hero from World of Warcraft')
---
+++ Basic/js/B3.js
@@ -0,0 +1,11 @@
+function persInfo(name, surname, age, job){
+	document.write("Hi! I am " + name + " " +surname+ " I am " +age+ " years old and I am a " + job + ".")
+}
+
+
+var name = "John";
+var surname = "Doe";
+var age = 25;
+var job = "future programmer";
+
+persInfo(name, surname, age, job)
